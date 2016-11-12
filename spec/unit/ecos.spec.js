345c3d5fc41bdb19b0e6fa4c4d8dd652ff9e3854
lajohnston <lajohnston@live.co.uk>
2016-11-12 16:14:44 +0000

    Add ecos.createEntity

---
+++ spec/unit/ecos.spec.js
@@ -0,0 +1,29 @@
+import Ecos from '../../src/Ecos';
+
+describe('Ecos', () => {
+  let ecos;
+  let entityFactory;
+
+  beforeEach(() => {
+    entityFactory = jasmine.createSpyObj('EntityFactory', ['create']);
+    ecos = new Ecos(entityFactory);
+  });
+
+  describe('createEntity', () => {
+    it('should return a new entity instance from the entity factory', () => {
+      const entity = {};
+      const components = {};
+
+      entityFactory.create.and.returnValue(entity);
+
+      const result = ecos.createEntity(components);
+
+      expect(entityFactory.create).toHaveBeenCalledWith(components);
+      expect(result).toBe(entity);
+    });
+  });
+
+  it('should create entities', () => {
+
+  });
+});
