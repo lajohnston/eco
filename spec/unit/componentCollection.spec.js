e932f9598d728489aabd63a6a61151f992abc942
lajohnston <lajohnston@live.co.uk>
2016-11-13 21:06:29 +0000

    Add ComponentCollection.add()

--- spec/unit/componentCollection.spec.js
+++ spec/unit/componentCollection.spec.js
@@ -1,8 +1,31 @@
-import ComponentCollection from '../../src/componentCollection';
+import ComponentCollection from '../../src/ComponentCollection';
 
 describe('ComponentCollection', () => {
+  let collection;
+  let componentFactory;
+
+  beforeEach(() => {
+    componentFactory = jasmine.createSpyObj('componentFactory', ['add']);
+    collection = new ComponentCollection(componentFactory);
+  });
+
   it('should define a constructor', () => {
     expect(ComponentCollection).toBeDefined();
     expect(new ComponentCollection()).toBeDefined();
+  });
+
+  describe('add()', () => {
+    it('should add the definition to the component factory', () => {
+      const definition = {};
+      collection.add('foo', definition);
+      expect(componentFactory.add).toHaveBeenCalledWith('foo', definition);
+    });
+
+    [true, false].forEach((result) => {
+      it('should return the result from the component factory', () => {
+        componentFactory.add.and.returnValue(result);
+        expect(collection.add('foo', {})).toBe(result);
+      });
+    });
   });
 });
--- src/ComponentCollection.js
+++ src/ComponentCollection.js
@@ -1,3 +1,19 @@
 export default class ComponentCollection {
+  constructor(componentFactory) {
+    this.componentFactory = componentFactory;
+    this.componentEntities = {};
+  }
 
+  /**
+   * Adds a component definition
+   *
+   * @param {string}  name  the unique name for the component
+   * @param {mixed}   data  the default data for the component
+   *
+   * @returns {boolean} true if the component was added successfully,
+   *                    or false if it was invalid or already exists
+   */
+  add(name, data) {
+    return this.componentFactory.add(name, data);
+  }
 }
