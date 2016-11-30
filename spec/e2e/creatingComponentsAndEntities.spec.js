6cae5201027aea557f27cc955efa44f10c191bd1
lajohnston <lajohnston@live.co.uk>
2016-11-30 10:17:17 +0000

    Clarify e2e spec definitions

--- spec/e2e/creatingComponentsAndEntities.spec.js
+++ spec/e2e/creatingComponentsAndEntities.spec.js
@@ -1,11 +1,11 @@
-describe('Adding components', () => {
+describe('Adding components and entities', () => {
   let eco;
 
   beforeEach(() => {
     eco = new window.Eco();
   });
 
-  describe('eco.addComponent()', () => {
+  describe('Adding new components with eco.addComponent()', () => {
     it('should return a component instance that stores data for entities', () => {
       const component = eco.addComponent('foo', {});
 
@@ -15,47 +15,47 @@ describe('Adding components', () => {
       expect(component.get(1).foo).toBe('foo1');
       expect(component.get(2).foo).toBe('foo2');
     });
+  });
 
-    describe('Replacing components', () => {
-      let oldComponent;
-      let newComponent;
-      let entityA;
-      let entityB;
+  describe('Redefining existing components with eco.addComponent()', () => {
+    let oldComponent;
+    let newComponent;
+    let entityA;
+    let entityB;
 
-      beforeEach(() => {
-        oldComponent = eco.addComponent('foo', { componentName: 'oldFoo' });
-        entityA = eco.createEntity()
-          .add('foo', { entityName: 'entityA' });
+    beforeEach(() => {
+      oldComponent = eco.addComponent('foo', { componentName: 'oldFoo' });
+      entityA = eco.createEntity()
+        .add('foo', { entityName: 'entityA' });
 
-        newComponent = eco.addComponent('foo', { componentName: 'newFoo' });
+      newComponent = eco.addComponent('foo', { componentName: 'newFoo' });
 
-        entityB = eco.createEntity()
-          .add('foo', { entityName: 'entityB' });
-      });
+      entityB = eco.createEntity()
+        .add('foo', { entityName: 'entityB' });
+    });
 
-      it('should not change the original component', () => {
-        // The old component instance should still contain entityA's data
-        expect(oldComponent.get(1).entityName).toBe('entityA');
-        expect(oldComponent.get(2)).not.toBeDefined();
+    it('should not change the original component instance', () => {
+      // The old component instance should still contain entityA's data
+      expect(oldComponent.get(1).entityName).toBe('entityA');
+      expect(oldComponent.get(2)).not.toBeDefined();
 
-        // The new component should only contain entityB's data
-        expect(newComponent.get(1)).not.toBeDefined();
-        expect(newComponent.get(2).entityName).toBe('entityB');
-      });
+      // The new component should only contain entityB's data
+      expect(newComponent.get(1)).not.toBeDefined();
+      expect(newComponent.get(2).entityName).toBe('entityB');
+    });
 
-      it('should ensure existing entities no longer have the component, as the definition has changed', () => {
-        expect(entityA.has('foo')).toBe(false);
-      });
+    it('should ensure existing entities no longer claim to have the component, as the definition has changed', () => {
+      expect(entityA.has('foo')).toBe(false);
+    });
 
-      it('should use the new definition for new entities', () => {
-        expect(entityB.get('foo').componentName).toBe('newFoo');
-      });
+    it('should use the new definition for new entities', () => {
+      expect(entityB.get('foo').componentName).toBe('newFoo');
     });
   });
 
-  describe('Adding object literals as component definitions', () => {
+  describe('Components defined with object-literal default values', () => {
     [undefined, null, false, true, function foo() {}, 1, '', 'foo'].forEach((nonObject) => {
-      it('should use the default value if non-object data is passed for a component instance', () => {
+      it('should use the default data if a non-object data is given for a component instance', () => {
         const defaultData = {
           fooValue: 'defaultFooValue',
         };
@@ -70,7 +70,7 @@ describe('Adding components', () => {
       });
     });
 
-    it('should shallow merge default values using the object definition', () => {
+    it('should shallow merge default values into the given data', () => {
       const defaultData = {
         fooValue: 'defaultFooValue',
         barValue: 'defaultBarValue',
@@ -89,7 +89,7 @@ describe('Adding components', () => {
     });
   });
 
-  describe('Adding functions as component definitions', () => {
+  describe('Components defined with a function', () => {
     it('should call the function when creating new instances, and store the result', () => {
       const entityData = {};
 
@@ -105,7 +105,7 @@ describe('Adding components', () => {
     });
   });
 
-  describe('Not passing any value as the component definition', () => {
+  describe('Components defined by a name only', () => {
     [undefined, null, false, true, function foo() {}, 1, '', 'foo', {}].forEach((value) => {
       it('should store and return any data passed to it', () => {
         eco.addComponent('foo');
@@ -118,13 +118,13 @@ describe('Adding components', () => {
     });
   });
 
-  describe('Passing a non-object or function as the component definition', () => {
+  describe('Components defined by a primitive value', () => {
     [null, false, true, 1, '', 'foo'].forEach((constantValue) => {
-      it('should treat the definition as a constant and always return this value', () => {
+      it('should ignore data passed to it and always return the primitive value', () => {
         eco.addComponent('foo', constantValue);
 
         const entity = eco.createEntity()
-          .add('foo', 'data that will be ignored');
+          .add('foo', 'this data should be ignored');
 
         expect(entity.get('foo')).toBe(constantValue);
       });
