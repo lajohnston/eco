72899f8df2b0a0f8450d7f07919ed59c595e013f
lajohnston <lajohnston@live.co.uk>
2017-12-01 20:39:47 +0000

    Add creatingEcoInstances e2e spec

--- gulpfile.js
+++ gulpfile.js
@@ -10,24 +10,20 @@
  */
 
 /* eslint-disable import/no-extraneous-dependencies */
-const gulp = require('gulp');
-const plugins = require('gulp-load-plugins')();
-const KarmaServer = require('karma').Server;
-const path = require('path');
-const moment = require('moment');
+const gulp = require("gulp");
+const plugins = require("gulp-load-plugins")();
+const KarmaServer = require("karma").Server;
+const path = require("path");
+const moment = require("moment");
 
-const src = [
-  'src/components/Collection.js',
-  'src/**/*.js',
-  'src/index.js',
-];
+const src = ["src/**/*.js", "src/index.js"];
 
 const now = moment();
-const packageJson = require('./package.json');
+const packageJson = require("./package.json");
 
 const header = `/*!
  * eco.js - v${packageJson.version}
- * Built ${now.format('ddd, Do MMM YYYY HH:mm:ss UTC')}
+ * Built ${now.format("ddd, Do MMM YYYY HH:mm:ss UTC")}
  *
  * https://github.com/lajohnston/eco
  *
@@ -42,54 +38,73 @@ function karmaErrorHandler(err, done) {
     return;
   }
 
-  done(new plugins.util.PluginError('karma', {
-    message: 'Karma Tests failed',
-  }));
+  done(
+    new plugins.util.PluginError("karma", {
+      message: "Karma Tests failed"
+    })
+  );
 }
 
 /**
  * Simple bundler to bundle ES6 classes into one file, without using a module system
  */
 function bundleJs() {
-  return gulp.src(src)
-    .pipe(plugins.concat('eco.js'))
-    .pipe(plugins.replace(/import .* from '.*';/g, ''))
-    .pipe(plugins.replace('export default class', 'class'))
-    .pipe(plugins.replace(/export default .*/g, ''));
+  return gulp
+    .src(src)
+    .pipe(plugins.concat("eco.js"))
+    .pipe(plugins.replace(/import .* from '.*';/g, ""))
+    .pipe(plugins.replace("export default class", "class"))
+    .pipe(plugins.replace(/export default .*/g, ""));
 }
 
-gulp.task('lint', () => (
-  gulp.src('src/**/*.js')
+gulp.task("lint", () =>
+  gulp
+    .src("src/**/*.js")
     .pipe(plugins.eslint())
     .pipe(plugins.eslint.format())
-));
+);
 
-gulp.task('build', gulp.parallel('lint', () => {
-  const wrapper = '(function() { <%= contents %> window.Eco = function () { return createEcoInstance(); };})();';
+gulp.task(
+  "build",
+  gulp.parallel("lint", () => {
+    const wrapper =
+      "(function() { <%= contents %> window.Eco = function () { return createEcoInstance(); };})();";
 
-  return bundleJs()
-    .pipe(plugins.babel({
-      presets: ['es2015'],
-    }))
-    .pipe(plugins.wrap(wrapper))
-    .pipe(plugins.uglify())
-    .pipe(plugins.wrap(`${header} <%= contents %>`))
-    .pipe(plugins.rename('eco.min.js'))
-    .pipe(gulp.dest('dist'));
-}));
+    return bundleJs()
+      .pipe(
+        plugins.babel({
+          presets: ["es2015"]
+        })
+      )
+      .pipe(plugins.wrap(wrapper))
+      .pipe(plugins.uglify())
+      .pipe(plugins.wrap(`${header} <%= contents %>`))
+      .pipe(plugins.rename("eco.min.js"))
+      .pipe(gulp.dest("dist"));
+  })
+);
 
-gulp.task('test:unit', (done) => {
-  new KarmaServer({
-    configFile: path.join(__dirname, '/spec/karma.unit.conf.js'),
-    singleRun: false,
-  }, err => karmaErrorHandler(err, done)).start();
+gulp.task("test:unit", done => {
+  new KarmaServer(
+    {
+      configFile: path.join(__dirname, "/spec/karma.unit.conf.js"),
+      singleRun: false
+    },
+    err => karmaErrorHandler(err, done)
+  ).start();
 });
 
-gulp.task('test:e2e', gulp.series('build', (done) => {
-  new KarmaServer({
-    configFile: path.join(__dirname, '/spec/karma.e2e.conf.js'),
-    singleRun: true,
-  }, err => karmaErrorHandler(err, done)).start();
-}));
+gulp.task(
+  "test:e2e",
+  gulp.series("build", done => {
+    new KarmaServer(
+      {
+        configFile: path.join(__dirname, "/spec/karma.e2e.conf.js"),
+        singleRun: true
+      },
+      err => karmaErrorHandler(err, done)
+    ).start();
+  })
+);
 
-gulp.task('test', gulp.series('test:unit', 'test:e2e'));
+gulp.task("test", gulp.series("test:unit", "test:e2e"));
--- spec/karma.e2e.conf.js
+++ spec/karma.e2e.conf.js
@@ -1,43 +1,36 @@
 // Karma configuration
 
-module.exports = (config) => {
+module.exports = config => {
   config.set({
-    frameworks: [
-      'jasmine',
-      'source-map-support',
-    ],
+    frameworks: ["jasmine", "source-map-support"],
 
     // list of files / patterns to load in the browser
-    files: [
-      'dist/eco.min.js',
-      'spec/e2e/**/*.spec.js',
-    ],
+    files: ["../dist/eco.min.js", "e2e/**/*.spec.js"],
 
     // list of files to exclude
-    exclude: [
-    ],
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     preprocessors: {
-      'spec/**/*.js': ['babel'],
+      "**/*.js": ["babel"]
     },
 
     babelPreprocessor: {
       options: {
-        presets: ['es2015'],
-        plugins: ['transform-es2015-modules-umd'],
-        sourceMap: 'inline',
-      },
+        presets: ["es2015"],
+        plugins: ["transform-es2015-modules-umd"],
+        sourceMap: "inline"
+      }
     },
 
     // test results reporter to use
-    reporters: ['progress'],
+    reporters: ["progress"],
 
     // web server port
     port: 9876,
 
     // start these browsers
-    browsers: ['PhantomJS', 'Chrome'],
+    browsers: ["PhantomJS", "Chrome"],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -45,6 +38,6 @@ module.exports = (config) => {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity,
+    concurrency: Infinity
   });
 };
